5a0c7845cd65bfd916895ceea42fb2a1424acaba
xuxinJS <xinxu.cnjs@gmail.com>
2020-06-29 22:50:57 +0800

    0629 2250 update

--- 200726_sun_classification/train/run_test.sh
+++ 200726_sun_classification/train/run_test.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 TEST="/home/dls1/simple_data/data_0627/continuum/test"
-DST="/home/dls1/simple_data/test_log"
 MODEL="xception"
 WEIGHT="/home/dls1/Desktop/xception_ep23_vloss0.0805.h5"
 OUTPUT='/home/dls1/simple_data/test_log'
-BATCH=2
 GPU="0"
 
-python3 test.py -t $TEST -m $MODEL -pw $WEIGHT  \
--o $OUTPUT -gpu $GPU
+python3 test.py -t $TEST -m $MODEL -pw $WEIGHT -o $OUTPUT -gpu $GPU
 
 
--- 200726_sun_classification/train/test.py
+++ 200726_sun_classification/train/test.py
@@ -2,7 +2,6 @@ import os
 import sys
 import cv2
 
-from model_lib import *
 from glob import glob
 import numpy as np
 import datetime as dt
@@ -16,30 +15,31 @@ from os import path as op
 from keras import backend as K
 from matplotlib import pyplot as plt
 
+from model_lib import *
+
 # control CUDA/tensorflow log level
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # or any {'0', '1', '2'}
 
 
-def save_result(image, mask, c_gt, c_pred, save_name):
-    mask = cv2.resize(mask, (image.shape[1], image.shape[0]))
-    contours, _2 = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    vis_image = cv2.drawContours(image, contours, -1, (255, 0, 0), 2)
-    cv2.putText(vis_image, 'GT:%s' % c_gt, (5, 25), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), thickness=1)
-    cv2.putText(vis_image, 'Pred:%s' % c_pred, (5, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), thickness=1)
-    cv2.imwrite(save_name, vis_image)
-
-
-def test_generator(names, dim, process_input, batch=1):
-    """
-    dim:(height, width, channel)
-    """
-    for name in names:
-        image = cv2.imread(name)
-        infer_image = cv2.resize(image, (dim[1], dim[0]))
-        infer_image = process_input(infer_image)
-        infer_image = np.expand_dims(infer_image, axis=0)
-        actual_batch = 1
-        yield infer_image, actual_batch
+# def save_result(image, mask, c_gt, c_pred, save_name):
+#     mask = cv2.resize(mask, (image.shape[1], image.shape[0]))
+#     contours, _2 = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+#     vis_image = cv2.drawContours(image, contours, -1, (255, 0, 0), 2)
+#     cv2.putText(vis_image, 'GT:%s' % c_gt, (5, 25), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), thickness=1)
+#     cv2.putText(vis_image, 'Pred:%s' % c_pred, (5, 50), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), thickness=1)
+#     cv2.imwrite(save_name, vis_image)
+#
+# def test_generator(names, dim, process_input, batch=1):
+#     """
+#     dim:(height, width, channel)
+#     """
+#     for name in names:
+#         image = cv2.imread(name)
+#         infer_image = cv2.resize(image, (dim[1], dim[0]))
+#         infer_image = process_input(infer_image)
+#         infer_image = np.expand_dims(infer_image, axis=0)
+#         actual_batch = 1
+#         yield infer_image, actual_batch
 
 
 def build_argparser():
@@ -49,7 +49,6 @@ def build_argparser():
     parser.add_argument('-pw', help='pretrained_weights path', required=True, type=str)
     parser.add_argument('-t', '--test_dir', help='folder of the testing data', required=True, type=str)
     parser.add_argument('-o', help='Path to save log', type=str, default='.')
-    parser.add_argument('-acc', help='test accuracy', action='store_true')
     parser.add_argument('-gpu', default='0', type=str)
     return parser
 
@@ -63,12 +62,11 @@ if __name__ == '__main__':
     output_dir = op.join(op.abspath(args.o), start_time)
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
-    acc_flag = args.acc
     gpu = args.gpu
-    # label_file = args.label
     class_ids = {'alpha': 0, 'beta': 1, 'betax': 2}
     class_num = len(class_ids)
 
+    # label_file = args.label
     # label = open(label_file, 'r')
     # label_lines = label.readlines()
     # label.close()
@@ -76,17 +74,20 @@ if __name__ == '__main__':
     # for i in label_lines:
     #     data = i.strip().split(' ')
     #     label_dict[data[0]] = int(data[1])
+
     test_names = []
-    test_labels = []
+    test_labels = []  # if not test acc,will be empty
     pred_labels = []
     for name in os.listdir(test_dir):
         full_name = op.join(test_dir, name)
         if op.isdir(full_name):
+            # test acc
             tmp_names = glob(op.join(full_name, '*'))
             tmp_indexs = [class_ids[name]] * len(tmp_names)
             test_names.extend(tmp_names)
             test_labels.extend(tmp_indexs)
         else:
+            # only save result
             test_names.append(full_name)
 
     # keras config
@@ -111,8 +112,8 @@ if __name__ == '__main__':
             model, process_input = xception(input_size=(image_height, image_width, 3),
                                             num_classes=class_num)
         model.load_weights(weight)
-        # image_generator = test_generator(test_names, (image_height, image_width), process_input)
 
+        # image_generator = test_generator(test_names, (image_height, image_width), process_input)
         # if save_folder:
         #     error_folder = op.join(save_folder, 'error')
         #     good_folder = op.join(save_folder, 'good')
@@ -132,34 +133,51 @@ if __name__ == '__main__':
             # print(name, init_result)
 
     # write tianchi update result
-    save_name = "C_whisper.txt"
-    full_save_name = op.join(output_dir, save_name)
-    with open(full_save_name, 'w') as f:
+    txt_name = "C_whisper.txt"
+    full_txt_name = op.join(output_dir, txt_name)
+    with open(full_txt_name, 'w') as f:
         for index, image_name in enumerate(test_names):
             simple_name, _ = op.splitext(op.basename(image_name))
             pred_label = pred_labels[index] + 1
             f.write("%s %d\n" % (simple_name, pred_label))
 
-    # write accuracy result
-    acc_name = "acc.txt"
-    full_acc_name = op.join(output_dir, acc_name)
     if len(test_labels):
+        # write accuracy result txt
+        acc_name = "acc.txt"
+        full_acc_name = op.join(output_dir, acc_name)
         with open(full_acc_name, 'w') as f:
             f.write('----------------confusion matrix----------------\n')
             cm = metrics.confusion_matrix(test_labels, pred_labels)
             f.write(str(cm))
+
+            f.write("\n\n----------precisions, recall, f1_score----------\n")
             precisions, recall, f1_score, _ = metrics.precision_recall_fscore_support(test_labels, pred_labels)
-            f.write("\n----------precisions, recall, f1_score----------\n")
             f.write("\tprecision\trecall\t\tf1_score\n")
-            labels = range(class_num)
-            for index, i in enumerate(labels):
-                f.write("%s\t%.5f\t\t%.5f\t\t%.5f\n" % (i, precisions[index], recall[index], f1_score[index]))
+            for i in range(class_num):
+                f.write("%s\t%.5f\t\t%.5f\t\t%.5f\n" % (i, precisions[i], recall[i], f1_score[i]))
 
             f.write("average precision:%.5f\n" % np.mean(precisions))
             f.write("average recall:%.5f\n" % np.mean(recall))
             f.write("average f1_score:%.5f\n" % np.mean(f1_score))
 
-        # show confusion metrics heamap
+        # save error result
+        index_arr = np.arange(len(test_names))
+        test_labels_arr = np.array(test_labels)
+        pred_labels_arr = np.array(pred_labels)
+        error_bool_index = (test_labels_arr != pred_labels_arr)
+        index_err = index_arr[error_bool_index]
+        error_folder = op.join(output_dir, 'error')
+        if not os.path.exists(error_folder):
+            os.makedirs(error_folder)
+        for index in index_err:
+            err_image_name = test_names[index]
+            err_img = cv2.imread(err_image_name)
+            cv2.putText(err_img, "GT:%d Pred:%d" % (test_labels_arr[index], pred_labels_arr[index]), (0, 30),
+                        cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), thickness=1)
+            save_image_name = op.join(error_folder, op.basename(err_image_name))
+            cv2.imwrite(save_image_name, err_img)
+
+        # plot show confusion metrics heamap
         sn.heatmap(cm, annot=True, cmap=plt.cm.Oranges, fmt="d")
         plt.ylabel('Ground Truth')
         plt.xlabel('Prediction')
